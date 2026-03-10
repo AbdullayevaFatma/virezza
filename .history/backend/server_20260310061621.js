04fea82e54d9538bc0e4b77bb7adbe8339f96df7
Fatma <fatma.abdullayewa@gmail.com>
2026-03-10 06:36:55 +0300

    db.js

---
+++ .history/backend/server_20260310061621.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const connectDB = requ
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+dotenv.config()
+
+const PORT = process.env.PORT || 9000
+
+app.get("/",(req,res)=>{res.send("WELCOME")})
+
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)})
