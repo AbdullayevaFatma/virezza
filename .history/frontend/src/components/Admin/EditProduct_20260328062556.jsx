c71aaeb615c7a96adc9b356d28fc583bd2851eea
Fatma <fatma.abdullayewa@gmail.com>
2026-03-28 06:47:24 +0300

    update components to fetch data and integrate redux instead of mock data

---
+++ .history/frontend/src/components/Admin/EditProduct_20260328062556.jsx
@@ -0,0 +1,193 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchProductDetails } from "../../redux/slices/productsSlice";
+
+const EditProduct = () => {
+const dispatch = useDispatch()
+const navigate = useNavigate()
+const {id} = useParams()
+const {selectedProduct,loading,error} = useSelector((state)=>state.products)
+
+  const [productData, setProductData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    countInStock: 0,
+    sku: "",
+    category: "",
+    brand: "",
+    sizes: [],
+    colors: [],
+    collections: "",
+    material: {},
+    gender: "",
+    images: [],
+  });
+
+  const [uploading,setUploading] = useState(false)
+
+  useEffect(()=>{
+    if(id){
+      dispatch(fetchProductDetails(id))
+    }
+  },[dispatch,id])
+
+  useEffect(()=>{
+    if(selectedProduct){
+      setProductData(selectedProduct)
+    }
+  },[selectedProduct])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProductData((prevData) => ({ ...prevData, [name]: value }));
+  };
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new formData()
+    formData.append("image",file)
+
+    try {
+      setUploading(true)
+    } catch (error) {
+      
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(productData);
+  };
+  return (
+    <div className="max-w-5xl mx-auto p-6 shadow-md">
+      <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Product Name</label>
+          <input
+            type="text"
+            name="name"
+            value={productData.name}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Description</label>
+          <textarea
+            onChange={handleChange}
+            name="description"
+            value={productData.description}
+            className="w-full border border-gray-300 p-2"
+            rows={4}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Price</label>
+          <input
+            type="number"
+            name="price"
+            value={productData.price}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Count In Stock</label>
+          <input
+            type="number"
+            name="countInStock"
+            value={productData.countInStock}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">SKU</label>
+          <input
+            type="text"
+            name="sku"
+            value={productData.sku}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">
+            Sizes (comma-separated)
+          </label>
+          <input
+            type="text"
+            name="sizes"
+            value={productData.sizes.join(",")}
+            onChange={(e) =>
+              setProductData({
+                ...productData,
+                sizes: e.target.value.split(",").map((size) => size.trim()),
+              })
+            }
+            className="w-full border border-gray-300 p-2"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">
+            Colors (comma-separated)
+          </label>
+          <input
+            type="text"
+            name="colors"
+            value={productData.colors.join(",")}
+            onChange={(e) =>
+              setProductData({
+                ...productData,
+                colors: e.target.value.split(",").map((color) => color.trim()),
+              })
+            }
+            className="w-full border border-gray-300 p-2"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Upload Image</label>
+          <input
+            type="file"
+            onChange={handleImageUpload}
+            className="block w-full text-sm text-gray-500
+  file:mr-4 file:py-2 file:px-4
+  file:cursor-pointer file:border file:border-gray-300
+  file:text-sm file:font-semibold
+  file:bg-gray-100 file:text-gray-700
+  hover:file:bg-gray-200"
+          />
+          <div className="flex gap-4 mt-4">
+            {productData.images.map((image, index) => (
+              <div className="" key={index}>
+                <img
+                  src={image.url}
+                  alt={image.altTeaxt || "Product Image"}
+                  className="w-20 h-20 object-cover shadow-md"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-green-500 text-white py-2 hover:bg-green-700 transition-colors cursor-pointer"
+        >
+          Update Product
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditProduct;
